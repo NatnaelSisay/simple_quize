b8bf93e4218054ce173906368f3b08634147fa53
NatnaelSisay <natyjava8@gmail.com>
2021-03-08 10:32:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="question_progress"></div>
+                <div className="main_container">
+                    <div className="question">
+                        <div className="question_header"></div>
+                        <div className="question_body"></div>
+                        <div className="question_choices"></div>
+                    </div>
+
+                    <div className="answer">
+                        <p>Correct</p>
+                        <button>Next Question</button>
+                    </div>
+
+                    <div className="score"></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
